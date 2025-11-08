5b70f2fc44fa9fe87a2a41bd383598757bad0e8b
Bianca <bianca06112003@gmail.com>
2025-11-08 19:46:09 +0000

    main

--- fila.h
+++ fila.h
@@ -1,26 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
-    int id;
-    char nome[50];
+typedef struct {                                    //Define a struct para armazenar os dados do paciente.
+    int id;                                         //ID do paciente. -->IDENTIFICAÇÃO DO PACIENTE<--
+    char nome[50];                                  
 } paciente;
 
-typedef struct elemento_fila {
-    paciente dados;
-    struct fila* prox;
+typedef struct elemento_fila {                      //Define a struct para os elementos da fila.
+    paciente dados;                                 
+    struct elemento_fila* prox;                          
 } Elemento;
 
-typedef struct fila{
+typedef struct fila{                                //Define a struct para a fila em si. -->CONTROLE DA FILA<--
     Elemento* inicio;
     Elemento* fim;
     int tamanho;
 } Fila;
 
-
-Elemento* criar_fila();
+//protótipos das funções
+Fila* criar_fila();
+paciente adicionar_paciente(int id, const char* nome);
 int lista_vazia(Fila* fila);
 void liberar_fila(Fila* fila);
 void enfileirar(Fila* fila, paciente dados);
-void print_paciente(paciente c);
-paciente adicionar_paciente(int id, const char* nome);
+void imprimir_fila(Fila* fila);
+int tamanho_fila(Fila* fila);
+paciente desenfileirar(Fila* fila);
+void print_paciente(paciente paciente);
+void limpar_buffer();
